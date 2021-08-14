4f80a04995ebc827998b427f9c5db3616108ac35
Dukathproject <dukathproject@github.com>
2021-08-14 17:27:52 -0300

    Modelos User Ranking y Question

--- Trivia/models.py
+++ Trivia/models.py
@@ -2,7 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class Cliente(models.Model):
-    
-    user = models.CharField(max_length=200)
+class User(models.Model):
+    name = models.CharField(max_length=200, primary_key=True)
+    email = models.EmailField(max_length=200)
     password = models.CharField(max_length=200,blank=True)
+    
+class Question(models.Model):
+    question = models.CharField(max_length=200, null=False)
+    difficulty = models.IntegerField(null=False, default=1)
+    
+class Ranking(models.Model):
+    name = models.CharField(max_length=200, primary_key=True)
+    level = models.IntegerField(null=False, default=0)
