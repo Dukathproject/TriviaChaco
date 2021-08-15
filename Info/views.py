77e47bc39cc6ce50780b628c7818257e8df9d3fd
Dukathproject <dukathproject@github.com>
2021-08-15 00:48:25 -0300

    Registro de usuario funcional sin redireccionar

--- Info/views.py
+++ Info/views.py
@@ -6,30 +6,26 @@ from Trivia.forms import RegisterForm
 
 #conectar a la base de datos
 db=_mysql.connect(host="127.0.0.1",user="root", passwd="dukath1234",db="informatorio", port=3306)
-
-
 #----------------------------------------------------------------------
 #insertar en db
-# db.query("""INSERT INTO `informatorio`.`trivia_user` (`name`, `email`, `password`) VALUES ('test2', 'test2@gmail.com', 'pass123445');""")
 
 #consulta en db
-db.query("""SELECT * FROM informatorio.trivia_user;""")
-r=db.store_result()
-query_result = r.fetch_row(maxrows=0, how=1)
+# db.query("""SELECT * FROM informatorio.trivia_user;""")
+# r=db.store_result()
+# query_result = r.fetch_row(maxrows=0, how=1)
 
 #mostrar consulta
-for consulta in query_result:
-    print(consulta["id"])
-print(query_result[0]["name"])
+# for consulta in query_result:
+#     print(consulta["id"])
+# print(query_result[0]["name"])
 # db.query("""INSERT trivia_user
 # 	values ('Test', 'dukath@gmail.com', 'pass1234');""")
 #----------------------------------------------------------------------
 
-#agregar redirect para las rutas erroneas
 def index(request):
     return render(request, "index.html")
 
-#registracion
+#registrar usuario
 def register(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
@@ -38,9 +34,22 @@ def register(request):
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            print(form.cleaned_data)
-            # redirect to a new URL:
-            # return HttpResponseRedirect('/thanks/')
+            form_data = form.cleaned_data
+            #revisar en db si ya existe usuario
+            query_str = f"""SELECT name FROM informatorio.trivia_user
+                        WHERE name = '{form_data['name']}';""" 
+            db.query(query_str)
+            r=db.store_result()
+            query_result = r.fetch_row(maxrows=1)
+            if query_result:
+                print("usuario ya existente")
+                #redireccionar
+            else:
+                #si no existe usuario, se crea
+                print("usuario creado")
+                query_str = f"""INSERT INTO `informatorio`.`trivia_user` (`name`, `email`, `password`) VALUES ('{form_data['name']}', '{form_data['email']}','{form_data['password']}');"""
+                db.query(query_str)
+                #redireccionar
 
     # if a GET (or any other method) we'll create a blank form
     else:
