a51c540d4c606db1c5b241fa3c99933c40535da9
Dukathproject <dukathproject@github.com>
2021-08-18 00:08:42 -0300

    Modelos Admin

--- Trivia/admin.py
+++ Trivia/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Config_Partida, Pregunta, Respuesta, Eleccion_del_Usuario
+
 
 # Register your models here.
+admin.site.register(Config_Partida)
+admin.site.register(Pregunta)
+admin.site.register(Respuesta)
+admin.site.register(Eleccion_del_Usuario)
--- Trivia/models.py
+++ Trivia/models.py
@@ -1,16 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+import random
 
-# Create your models here.
-
-class User(models.Model):
-    name = models.CharField(max_length=200, primary_key=True)
-    email = models.EmailField(max_length=200)
-    password = models.CharField(max_length=200,blank=True)
+class Config_Partida(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=500)    
+    numero_de_preguntas = models.IntegerField(default=1)
+    tiempo = models.IntegerField(help_text="Duración de la trivia en segundos", default="1")
     
-class Question(models.Model):
-    question = models.CharField(max_length=200, null=False)
-    difficulty = models.IntegerField(null=False, default=1)
+    def __str__(self):
+        return self.nombre
     
-class Ranking(models.Model):
-    name = models.CharField(max_length=200, primary_key=True)
-    level = models.IntegerField(null=False, default=0)
+    def get_preguntas(self):
+        return self.preguntas_set.all()
+    
+class Pregunta(models.Model):
+    formula = models.CharField(max_length=200)
+    trivia = models.ForeignKey(Config_Partida, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.formula
+    
+    def get_respuestas(self):
+        return self.respuestas_set.all()
+    
+    
+class Respuesta(models.Model):
+    formula = models.CharField(max_length=200)
+    correcta = models.BooleanField(default=False)
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"Pregunta: {self.pregunta.formula}, answer: {self.formula}, correct: {self.correcta}"
+    
+class Eleccion_del_Usuario(models.Model):
+    trivia = models.ForeignKey(Config_Partida, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    aciertos = models.FloatField()
+    
+    def __str__(self):
+        return str(self.trivia)
