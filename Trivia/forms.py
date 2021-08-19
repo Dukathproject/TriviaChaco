6da7c7564750d27e347cb13fb2a9a636652eb2a8
Dukathproject <dukathproject@github.com>
2021-08-19 00:18:12 -0300

    Contraseña escondida al loguear y registrarse

--- Trivia/forms.py
+++ Trivia/forms.py
@@ -3,8 +3,8 @@ from django import forms
 class RegisterForm(forms.Form):
     name = forms.CharField(label='name', max_length=100)
     email = forms.EmailField(label='email')
-    password = forms.CharField(label='password',max_length=100)
+    password = forms.CharField(max_length=32, widget=forms.PasswordInput)
     
 class LoginForm(forms.Form):
     name = forms.CharField(label='name', max_length=100)
-    password = forms.CharField(label='password',max_length=100)
+    password = forms.CharField(max_length=32, widget=forms.PasswordInput)
